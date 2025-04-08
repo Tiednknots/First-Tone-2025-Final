c5051827b88d08f2ead2796b41179f9f9ba08b37
Jonathan Barnes <create@jonathanbarnes.work>
2025-04-07 22:36:05 -0700

    dsfsd

--- theme/assets/js/hidden.js
+++
@@ -1,9 +0,0 @@
-<script>
-  window.addEventListener("DOMContentLoaded", function () {
-    const banner = document.querySelector("body > udesly-banner");
-    if (banner) {
-      banner.remove(); // 💥 Gone!
-      console.log("🔥 udesly-banner removed");
-    }
-  });
-</script>
